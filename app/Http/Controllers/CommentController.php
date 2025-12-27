c586e58a413b6e57a98e5b6afd295e7385126a41
Adem-elwafi <adem.elwafi.dev@gmail.com>
2025-12-27 13:01:04 +0100

    feat(comments): add CommentController with store method

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class CommentController extends Controller
+{
+
+public function store(Request $request, Post $post)
+{
+    $request->validate([
+        'body' => 'required|string|max:1000',
+    ]);
+
+    Comment::create([
+        'body' => $request->input('body'),
+        'user_id' => Auth::id(),
+        'post_id' => $post->id,
+    ]);
+
+    return back()->with('success', 'Comment added successfully.');
+}
+
+}
