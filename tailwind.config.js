3bbc1baff494f9b848d847afd39ad6d408c3660b
Adem-elwafi <adem.elwafi.dev@gmail.com>
2026-01-03 12:20:04 +0100

    config: add React components to Tailwind content paths
    - Include resources/js/**/*.{js,jsx} in Tailwind scanning
    - Set darkMode to 'class' for programmatic dark mode control
    - Prevents purging of Tailwind classes used in React components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,16 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-
-    plugins: [forms],
-};
+module.exports = {
+  content: [
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.{js,jsx,ts,tsx}',
+    './vendor/livewire/livewire/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
