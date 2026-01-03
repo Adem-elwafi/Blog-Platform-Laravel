287f6f93edfbbdfcd0e5895effff0d9e87493a48
Adem-elwafi <adem.elwafi.dev@gmail.com>
2026-01-03 12:59:41 +0100

    config: add CSRF token and baseURL to axios defaults
    - Automatically include X-CSRF-TOKEN header from meta tag
    - Set baseURL to '/' for cleaner API calls
    - Enables React components to make authenticated requests

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,8 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// After creating axios instance
+window.axios.defaults.baseURL = '/';
+window.axios.defaults.headers.common['X-CSRF-TOKEN'] = 
+    document.querySelector('meta[name="csrf-token"]')?.content;
