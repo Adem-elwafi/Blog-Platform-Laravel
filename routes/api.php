edf16eaee2dce5411e113604cdfcb3eedd75845d
Adem-elwafi <adem.elwafi.dev@gmail.com>
2026-01-03 13:07:33 +0100

    api: add comment endpoints for React integration
    - GET /api/posts/{post}/comments - fetch comments
    - POST /api/posts/{post}/comments - create comment
    - DELETE /api/comments/{comment} - delete comment
    - All routes use auth:sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,26 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\LikeController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
-Route::middleware('auth:sanctum')->post(
-    '/posts/{post}/like',
-    [LikeController::class, 'toggle']
-);
-
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/posts/{post}/like', [LikeController::class, 'store']);
+    Route::delete('/posts/{post}/like', [LikeController::class, 'destroy']);
+    
+    // Comment API routes
+    Route::get('/posts/{post}/comments', [CommentController::class, 'index']);
+    Route::post('/posts/{post}/comments', [CommentController::class, 'apiStore']);
+    Route::delete('/comments/{comment}', [CommentController::class, 'apiDestroy']);
+});
